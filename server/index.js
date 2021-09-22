7893aa00e66f0077060c619ad0727aac628b4ecc
itgitnit <Thakurnikhil2210@gmail.com>
2021-09-21 21:39:37 -0700

    Memory_Creator_First_Commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+// import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/posts.js';
+
+// const express = require('express')
+const app = express();
+
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://memory_2210:memory2210@cluster0.ikqls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
+
+// https://www.mongodb.com/cloud/atlas
+
+// After 15 min
